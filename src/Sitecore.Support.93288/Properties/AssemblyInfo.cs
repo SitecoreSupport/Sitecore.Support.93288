a4c178db40439afb6dd984703e33d716eae4e27b
Paul Stupka <paul.stupka@sitecore.com>
2019-05-28 11:06:09 +0300

    Update solution for inclusion of a fix for 323484

--- src/Sitecore.Support.93288/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.93288/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Sitecore.Support.93288")]
-[assembly: AssemblyProduct("Sitecore.Support.93288")]
+[assembly: AssemblyTitle("Sitecore.Support.93288.323484")]
+[assembly: AssemblyProduct("Sitecore.Support.93288.323484")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyDescription("WebDeploy: Publishing gets stuck at initialization; administrator account credentials are specified in the WebDeploy.config file")]
+
